42eb9f843e456eaf18a34c226f60c9be6fc199b5
Mark Wassef <markdevw@gmail.com>
2015-03-26 17:53:44 -0400

    Initial commit

---
+++ app/src/main/java/com/gmail/markdevw/wetube/activities/MainActivity.java
@@ -0,0 +1,20 @@
+package com.gmail.markdevw.wetube.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.gmail.markdevw.wetube.R;
+
+/**
+ * Created by Mark on 3/24/2015.
+ */
+
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
